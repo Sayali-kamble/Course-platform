e0c8590324265e822fd4a4885f76fb71d0763759
Sayali-kamble <sayalikamble4481@gmail.com>
2026-01-26 15:18:17 +0530

    Initial spring boot project setup

---
+++ src/test/java/com/edtech/course_platform/CoursePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edtech.course_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
