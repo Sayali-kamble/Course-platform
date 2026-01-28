ec1129559639c70c0d97c706a4fc649baabc32ca
Sayali-kamble <sayalikamble4481@gmail.com>
2026-01-28 15:39:35 +0530

    implement spring security configuration

---
+++ src/main/java/com/edtech/course_platform/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.edtech.course_platform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/courses",
+                                "/api/courses/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/edtech/course_platform/service/CourseService.java
@@ -0,0 +1,2 @@
+package com.edtech.course_platform.service;public class CourseService {
+}
