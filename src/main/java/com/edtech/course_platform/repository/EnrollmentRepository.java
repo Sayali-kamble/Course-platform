6828a0348c40aa7539fc340dd5f26dc72e1eabd7
Sayali-kamble <sayalikamble4481@gmail.com>
2026-02-02 22:56:38 +0530

    implement course enrollment api

--- src/main/java/com/edtech/course_platform/repository/EnrollmentRepository.java
+++ src/main/java/com/edtech/course_platform/repository/EnrollmentRepository.java
@@ -5,7 +5,6 @@ import com.edtech.course_platform.entity.Enrollment;
 import com.edtech.course_platform.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
