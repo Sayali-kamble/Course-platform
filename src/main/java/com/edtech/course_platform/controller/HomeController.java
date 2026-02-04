d395a8896a0d75f43af6b5f2c85dfff1674f868b
Sayali-kamble <sayalikamble4481@gmail.com>
2026-02-04 11:18:21 +0530

    update file

--- src/main/java/com/edtech/course_platform/controller/HomeController.java
+++ src/main/java/com/edtech/course_platform/controller/HomeController.java
@@ -1,2 +1,13 @@
-package com.edtech.course_platform.controller;public class HomeController {
+package com.edtech.course_platform.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Course Platform API Home Page";
+    }
 }
