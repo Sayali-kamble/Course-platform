49b8b98561f732436dc07fc8ff10ee1bfe13b553
Sayali-kamble <sayalikamble4481@gmail.com>
2026-02-04 15:55:38 +0530

    update enrollment api

--- src/main/java/com/edtech/course_platform/service/impl/EnrollmentServiceImpl.java
+++ src/main/java/com/edtech/course_platform/service/impl/EnrollmentServiceImpl.java
@@ -16,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
+
 
 @Service
 @RequiredArgsConstructor
